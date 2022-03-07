044560ec6cd98b5935bc1ff34a7e929ba5b9ca95
dannielgloria <danniel.gloria@gmail.com>
2022-03-07 02:37:34 -0600

    ok

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/PruebaDev'));
+app.use(express.static(__dirname + '/dist/beMG'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/PruebaDev/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/beMG/index.html'));
 });
 
 // default Heroku port
